cd6c2da536bfbc0801165f9ce66278715d73236f
nosun <nosun2008@126.com>
2016-07-07 20:30:44 +0800

    update words to chinese

--- resources/views/parts/topBar.blade.php
+++ resources/views/parts/topBar.blade.php
@@ -28,8 +28,8 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
+                    <li><a href="{{ url('/login') }}">登录</a></li>
+                    <li><a href="{{ url('/register') }}">注册</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -37,7 +37,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>登出</a></li>
                         </ul>
                     </li>
                 @endif
